99bbb268050095a442191c90bbaf8812c3689e71
MarianoEs <marianoesser@gmail.com>
2026-06-13 21:45:05 -0300

    Creando base de datos

---
+++ TuBebe.BD/Datos/AppDbContex.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TuBebe.BD.Datos.Entity;
+
+namespace TuBebe.BD.Datos
+{
+    public class AppDbContex : DbContext
+    {
+        public DbSet<Pantalon> Pantalones { get; set; }
+
+        public AppDbContext(DbContextOptions options) : base(options) 
+        { }
+
+
+    }
+}
--- TuBebe/TuBebe/Program.cs
+++ TuBebe/TuBebe/Program.cs
@@ -1,14 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using TuBebe.BD.Datos;
 using TuBebe.Client.Pages;
 using TuBebe.Components;
 
 var builder = WebApplication.CreateBuilder(args);
+
+#region Configurar el constructor de la app
+
+var connectionString = builder.Configuration.GetConnectionString("ConnSqlServer")
+    ?? throw new InvalidOperationException("Fallo la conexion a la base de datos");
+
+builder.Services.AddDbContext<AppDbContex>(options =>options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents()
     .AddInteractiveWebAssemblyComponents();
 
+
+#endregion
+
 var app = builder.Build();
+
+
+#region Crea la aplicacion y arranca los servicios
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
@@ -32,4 +47,8 @@ app.MapRazorComponents<App>()
     .AddInteractiveWebAssemblyRenderMode()
     .AddAdditionalAssemblies(typeof(TuBebe.Client._Imports).Assembly);
 
+#endregion
+
+
+//Arranca la aplicacion 
 app.Run();
